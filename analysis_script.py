eb8fa7ed12e68d0feb7ce63fa1478cc122fabbe0
Kartik Shenoy <kartik.shenoyy@gmail.com>
2022-05-11 18:55:21 -0500

    baseline models execution

--- analysis_script.py
+++ analysis_script.py
@@ -64,69 +64,69 @@ def execute_scenario(pows, types, file, runname, parent_analysis, bypass_exec=Fa
     print(pd.Series(times).describe())
     parent_analysis.append((runname, power_total_wins, power_solo_wins, power_shared_wins, power_solo_durations, power_shared_durations))
 
-# # if args.comb == 0:
-# # All Dipnets
-# pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
-# types = 're_np,re_np,re_np,re_np,re_np,re_np,re_np'
-# runname = 'All_dip_np'
-# file = runname + "_" + suffix + ".json"
-# execute_scenario(pows, types, file, runname, parent_analysis, True)
+# if args.comb == 0:
+# All Dipnets
+pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
+types = 're_np,re_np,re_np,re_np,re_np,re_np,re_np'
+runname = 'All_dip_np'
+file = runname + "_" + suffix + ".json"
+execute_scenario(pows, types, file, runname, parent_analysis, True)
 
-# # elif args.comb == 1:
-# # Germ Leader, Italy Follower All Dipnets
-# pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
-# types = 're_np,re_np,rlsp,re_np,rlspm,re_np,re_np'
-# runname = 'All_dip_Ger_Ita'
-# file = runname + "_" + suffix + ".json"
-# execute_scenario(pows, types, file, runname, parent_analysis, True)
+# elif args.comb == 1:
+# Germ Leader, Italy Follower All Dipnets
+pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
+types = 're_np,re_np,rlsp,re_np,rlspm,re_np,re_np'
+runname = 'All_dip_Ger_Ita'
+file = runname + "_" + suffix + ".json"
+execute_scenario(pows, types, file, runname, parent_analysis, True)
 
-# # elif args.comb == 2:
-# # Aus Leader, Italy Follower All Dipnets
-# pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
-# types = 're_np,rlspm,rlsp,re_np,re_np,re_np,re_np'
-# runname = 'All_dip_Aus_Ita'
-# file = runname + "_" + suffix + ".json"
-# execute_scenario(pows, types, file, runname, parent_analysis, True)
+# elif args.comb == 2:
+# Aus Leader, Italy Follower All Dipnets
+pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
+types = 're_np,rlspm,rlsp,re_np,re_np,re_np,re_np'
+runname = 'All_dip_Aus_Ita'
+file = runname + "_" + suffix + ".json"
+execute_scenario(pows, types, file, runname, parent_analysis, True)
 
-# # elif args.comb == 3:
-# # Eng Leader, France Follower All Dipnets
-# pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
-# types = 'rlspm,re_np,re_np,rlsp,re_np,re_np,re_np'
-# runname = 'All_dip_Eng_Fra'
-# file = runname + "_" + suffix + ".json"
-# execute_scenario(pows, types, file, runname, parent_analysis, True)
+# elif args.comb == 3:
+# Eng Leader, France Follower All Dipnets
+pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
+types = 'rlspm,re_np,re_np,rlsp,re_np,re_np,re_np'
+runname = 'All_dip_Eng_Fra'
+file = runname + "_" + suffix + ".json"
+execute_scenario(pows, types, file, runname, parent_analysis, True)
 
-# # elif args.comb == 4:
-# # France Leader, Eng Follower All Dipnets
-# pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
-# types = 'rlsp,re_np,re_np,rlspm,re_np,re_np,re_np'
-# runname = 'All_dip_Fra_Eng'
-# file = runname + "_" + suffix + ".json"
-# execute_scenario(pows, types, file, runname, parent_analysis, True)
+# elif args.comb == 4:
+# France Leader, Eng Follower All Dipnets
+pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
+types = 'rlsp,re_np,re_np,rlspm,re_np,re_np,re_np'
+runname = 'All_dip_Fra_Eng'
+file = runname + "_" + suffix + ".json"
+execute_scenario(pows, types, file, runname, parent_analysis, True)
 
-# # elif args.comb == 5:
-# # Aus Dipnet, All others random
-# pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
-# types = 'rnp,re_np,rnp,rnp,rnp,rnp,rnp'
-# runname = 'All_rand_Aus_dip'
-# file = runname + "_" + suffix + ".json"
-# execute_scenario(pows, types, file, runname, parent_analysis, True)
+# elif args.comb == 5:
+# Aus Dipnet, All others random
+pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
+types = 'rnp,re_np,rnp,rnp,rnp,rnp,rnp'
+runname = 'All_rand_Aus_dip'
+file = runname + "_" + suffix + ".json"
+execute_scenario(pows, types, file, runname, parent_analysis, True)
 
-# # elif args.comb == 6:
-# # Eng Dipnet, All others random
-# pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
-# types = 're_np,rnp,rnp,rnp,rnp,rnp,rnp'
-# runname = 'All_rand_Eng_dip'
-# file = runname + "_" + suffix + ".json"
-# execute_scenario(pows, types, file, runname, parent_analysis, True)
+# elif args.comb == 6:
+# Eng Dipnet, All others random
+pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
+types = 're_np,rnp,rnp,rnp,rnp,rnp,rnp'
+runname = 'All_rand_Eng_dip'
+file = runname + "_" + suffix + ".json"
+execute_scenario(pows, types, file, runname, parent_analysis, True)
 
-# # elif args.comb == 7:
-# # Eng Leader, Tur Follower All Dipnets
-# pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
-# types = 'rlspm,re_np,re_np,re_np,re_np,re_np,rlsp'
-# runname = 'All_dip_Eng_Tur'
-# file = runname + "_" + suffix + ".json"
-# execute_scenario(pows, types, file, runname, parent_analysis, True)
+# elif args.comb == 7:
+# Eng Leader, Tur Follower All Dipnets
+pows = 'ENGLAND,AUSTRIA,ITALY,FRANCE,GERMANY,RUSSIA,TURKEY'
+types = 'rlspm,re_np,re_np,re_np,re_np,re_np,rlsp'
+runname = 'All_dip_Eng_Tur'
+file = runname + "_" + suffix + ".json"
+execute_scenario(pows, types, file, runname, parent_analysis, True)
 
 # elif args.comb == 8:
 # Tur Leader, Eng Follower All Dipnets
