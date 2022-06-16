73553f19a80a6b14b12875ac3b0bf4d5719ebbc9
wwongkamjan <w.wongkamjan@gmail.com>
2022-06-15 22:13:52 -0700

    RL added

--- bots/RL/RLProposerBot.py
+++ bots/RL/RLProposerBot.py
@@ -37,30 +37,26 @@ class RLProposerBot(RLOrderBot):
         for recipient in self.game.powers:
             proposal_list = []
             if self.power_name != recipient and not self.powers[recipient].is_eliminated():
-                possible_orders = self.game.get_all_possible_orders()
-                orders_location = {loc: possible_orders[loc] for loc in
-                    self.game.get_orderable_locations(recipient) if possible_orders[loc]}
+                orders = yield self.brain.get_orders(self.game, recipient)
                 stance = self.env.dip_player.stance[self.power_name][recipient] 
                 self.env.set_power_state(self.power_name, stance)
-                for loc, orders in orders_location.items():
-                    n = len(orders)
-                    for order in orders[:min(self.K_ORDERS,n)]:
-                        #state = no order in consideation
-                        self.maa2c.env_state = dict_to_arr(self.env.cur_obs, self.maa2c.n_agents)
-                        action = self.maa2c.exploration_action(self.maa2c.env_state)
-                        action_dict = {agent_id: action[agent_id] for agent_id in range(self.maa2c.n_agents)}
-                        self.env.step(action_dict, self.power_name, recipient, order)
+                n = len(orders)
+                for order in orders[:min(self.K_ORDERS,n)]:
+                    #state = no order in consideation
+                    self.maa2c.env_state = dict_to_arr(self.env.cur_obs, self.maa2c.n_agents)
+                    action = self.maa2c.exploration_action(self.maa2c.env_state)
+                    action_dict = {agent_id: action[agent_id] for agent_id in range(self.maa2c.n_agents)}
+                    self.env.step(action_dict, self.power_name, recipient, order)
 
-                        #state = considering order
-                        self.maa2c.env_state = dict_to_arr(self.env.cur_obs, self.maa2c.n_agents)
-                        action = self.maa2c.exploration_action(self.maa2c.env_state)
-                        action_dict = {agent_id: action[agent_id] for agent_id in range(self.maa2c.n_agents)}
+                    #state = considering order
+                    self.maa2c.env_state = dict_to_arr(self.env.cur_obs, self.maa2c.n_agents)
+                    action = self.maa2c.exploration_action(self.maa2c.env_state)
+                    action_dict = {agent_id: action[agent_id] for agent_id in range(self.maa2c.n_agents)}
+                    self.env.step(action_dict, self.power_name, recipient, order)
+                    # if action=propose, we add it to the list
+                    if action_dict[self.env.power_mapping[self.power_name]]==1:
                         self.env.step(action_dict, self.power_name, recipient, order)
-                        # if action=propose, we add it to the list
-                        if action_dict[self.env.power_mapping[self.power_name]]==1:
-                            self.env.step(action_dict, self.power_name, recipient, order)
-                            proposal_list.append(order)
-                            break
+                        proposal_list.append(order)
                 if len(proposal_list)>0:
                     suggested_orders = ORR([XDO(order) for order in proposal_list])
                     ret_obj.add_message(recipient, str(suggested_orders))
@@ -82,7 +78,7 @@ def main():
     while not game.is_game_done:
         if game.phase_type =='M':
             bot.phase_init()
-            messages = bot.gen_messages(None).messages
+            messages = yield bot.gen_messages(None).messages
             for msg in messages:
                 msg_obj = Message(
                     sender=bot.power_name,
@@ -91,7 +87,7 @@ def main():
                     phase=game.get_current_phase(),
                 )
                 game.add_message(message=msg_obj)
-        bot_orders = bot.gen_orders()
+        bot_orders = yield bot.gen_orders()
         game.set_orders(bot_power, bot_orders)
         game.process()
     file_name = 'RLProposerBot.json'
