1aab5414acef07c8829ecf482562bbffa2c199d0
wwongkamjan <w.wongkamjan@gmail.com>
2022-06-19 23:36:58 -0700

    debugging

--- bots/dipnet/loyal_support_proposal.py
+++ bots/dipnet/loyal_support_proposal.py
@@ -400,6 +400,8 @@ class LSP_DipBot(DipnetBot):
                 for order in orders:
                     order_token = get_order_tokens(order) 
                     # print('check move if this is for ally or other power')
+                    if order== 'F SEV - BLA':
+                        self.is_move_for_ally(order)
                     if order_token[0] not in units and self.is_move_for_ally(order)[0]:
                         if order== 'F SEV - BLA':
                             print(order)
