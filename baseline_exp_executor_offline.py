3ec074358f3c8761913c4b8c37f8b44516b84aa9
Kartik Shenoy <kartik.shenoyy@gmail.com>
2022-06-21 18:43:20 -0500

    Temp change

--- baseline_exp_executor_offline.py
+++ baseline_exp_executor_offline.py
@@ -4,8 +4,8 @@ from tqdm import tqdm
 import time
 no_of_games = 100
 
-prefix = 'LSP_RUSTUR'
+prefix = 'NP_ALL'
 
 for i in range (no_of_games):
-    run(f"python3 botgame_launcher_dipnet.py -t np,np,np,np,np,rlspm,rlsp -f Analysis/{prefix}_{i}.json &", shell=True)
+    run(f"python3 botgame_launcher_dipnet.py -t np,np,np,np,np,np,np -f Analysis/{prefix}_{i}.json &", shell=True)
     time.sleep(60)
