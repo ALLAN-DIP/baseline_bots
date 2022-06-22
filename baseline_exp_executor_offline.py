73a6aa4452e6324df9df6cf0229dc440ca442c9e
wwongkamjan <w.wongkamjan@gmail.com>
2022-06-21 23:26:11 -0700

    debug

--- baseline_exp_executor_offline.py
+++ baseline_exp_executor_offline.py
@@ -4,16 +4,16 @@ from tqdm import tqdm
 import time
 no_of_games = 100
 
-prefix = 'LSP_RUSTUR_w_baselines'
-
-for i in range (no_of_games):
-    run(f"python3 botgame_launcher_dipnet.py -t push,np,tbt,stbt,np,rlspm,rlsp -f Analysis/{prefix}_{i}.json &", shell=True)
-    time.sleep(60)
-time.sleep(600)
-
-# prefix = 'LSP_RUSTUR'
+# prefix = 'LSP_RUSTUR_w_baselines'
 
 # for i in range (no_of_games):
-#     run(f"python3 botgame_launcher_dipnet.py -t np,np,np,np,np,rlspm,rlsp -f Analysis/{prefix}_{i}.json &", shell=True)
+#     run(f"python3 botgame_launcher_dipnet.py -t push,np,tbt,stbt,np,rlspm,rlsp -f Analysis/{prefix}_{i}.json &", shell=True)
 #     time.sleep(60)
 # time.sleep(600)
+
+prefix = 'LSP_RUSTUR'
+
+for i in range (no_of_games):
+    run(f"python3 botgame_launcher_dipnet.py -t np,np,np,np,np,rlspm,rlsp -f Analysis/{prefix}_{i}.json &", shell=True)
+    time.sleep(60)
+time.sleep(600)
