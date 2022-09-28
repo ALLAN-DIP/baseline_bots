1387f1b7f6242d27384bf71b4cc52082b3ed87d5
Kartik Shenoy <kartik.shenoyy@gmail.com>
2022-09-28 16:12:10 -0500

    feat: added function to send messages of orders to positive stance powers

--- src/baseline_bots/bots/smart_order_accepter_bot.py
+++ src/baseline_bots/bots/smart_order_accepter_bot.py
@@ -1,11 +1,12 @@
 __author__ = "Sander Schulhoff"
 __email__ = "sanderschulhoff@gmail.com"
 
-from DAIDE import ALY, PRP
+from DAIDE import FCT, XDO, ORR
 from diplomacy import Message
 
 from baseline_bots.bots.random_proposer_bot import RandomProposerBot
 from baseline_bots.utils import MessagesData, OrdersData, get_other_powers
+from baseline_bots.stance import stance_extraction
 
 # Using library https://github.com/SHADE-AI/daidepp
 from daidepp import create_daide_grammar, daide_visitor
@@ -26,9 +27,12 @@ class SmartOrderAccepterBot(RandomProposerBot):
     def __init__(self, power_name, game) -> None:
         super().__init__(power_name, game)
         self.alliance_props_sent = False
+        self.stance = stance_extraction.ScoreBasedStance()
 
-    def get_proposals(rcvd_messages) -> Dict[str, str]:
-        """Extract proposal messages from receieved messages and checks for valid syntax before returning it"""
+    def get_proposals(self, rcvd_messages) -> Dict[str, str]:
+        """
+        Extract proposal messages from received messages and checks for valid syntax before returning it
+        """
         grammar = create_daide_grammar(level=130)
         
         # Extract messages containing PRP string
@@ -40,7 +44,7 @@ class SmartOrderAccepterBot(RandomProposerBot):
                 # Parse using DAIDEPP functions
                 parse_tree = grammar.parse(order_msg.message)
                 output = daide_visitor.visit(parse_tree)
-                
+
                 if output[0] == 'PRP':
                     proposals[order_msg.sender] = order_msg.message
             except Exception as e:
@@ -48,6 +52,13 @@ class SmartOrderAccepterBot(RandomProposerBot):
                 pass
 
         return proposals
+
+    def gen_pos_stance_messages(self, game_rec, ret_msgs):
+        stance_vec = self.stance.get_stance(game_rec)
+        orders_decided = FCT(ORR(XDO(self.orders.get_list_of_orders())))
+        for pow in stance_vec[self.power_name]:
+            if stance_vec[self.power_name][pow] > 0:
+                ret_msgs.add_message(pow, str(orders_decided))
 
     def gen_messages(self, rcvd_messages):
         ret_msgs = MessagesData()
