4609a5d428044d297f3f5cf1b945dead0c7acff5
Wichayaporn Wongkamjan <w.wongkamjan@gmail.com>
2022-09-28 12:12:11 -0500

    test import utils

--- src/baseline_bots/utils.py
+++ src/baseline_bots/utils.py
@@ -14,7 +14,7 @@ from typing import List
 from DAIDE.utils.exceptions import ParseError
 from diplomacy import Game, Message
 from tornado import gen
-from baseline_bots.bots.baseline_bot import BaselineBot
+from baseline_bots import BaselineBot
 
 
 def get_order_tokens(order):
