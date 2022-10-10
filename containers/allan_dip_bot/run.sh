38bdbc57f4cced6d3c5f78d0e0f347f6f580bf08
Kartik Shenoy <kartik.shenoyy@gmail.com>
2022-10-10 01:58:29 -0500

    resolved paths

--- containers/allan_dip_bot/run.sh
+++ containers/allan_dip_bot/run.sh
@@ -6,4 +6,4 @@
 export PYTHONPATH=$PYTHONPATH:/model/src/model_server/research/
 
 #launch dipnet script
-python /model/src/model_server/research/run_dipnet.py $@
+python /model/src/model_server/baseline_bots/run_dipnet.py $@
--- containers/allan_dip_bot/run_model_server.sh
+++ containers/allan_dip_bot/run_model_server.sh
@@ -7,12 +7,12 @@ printf "batch_timeout_micros { value: %s }\n" $BATCH_TIMEOUT_MICROS >> $batch_fi
 printf "max_enqueued_batches { value: %s }\n" $MAX_ENQUEUED_BATCHES >> $batch_file
 printf "num_batch_threads { value: %s }\n" $NUM_BATCH_THREADS >> $batch_file
 printf "pad_variable_length_inputs: %s\n" $PAD_VARIABLE_LENGTH_INPUTS >> $batch_file
-printf "Running ALLAN bot"
-printf "    ___    __    __    ___    _   __"
-printf "   /   |  / /   / /   /   |  / | / /"
-printf "  / /| | / /   / /   / /| | /  |/ / "
-printf " / ___ |/ /___/ /___/ ___ |/ /|  /  "
-printf "/_/  |_/_____/_____/_/  |_/_/ |_/   "
+printf "Running ALLAN bot\n"
+printf "    ___    __    __    ___    _   __\n"
+printf "   /   |  / /   / /   /   |  / | / /\n"
+printf "  / /| | / /   / /   / /| | /  |/ / \n"
+printf " / ___ |/ /___/ /___/ ___ |/ /|  /  \n"
+printf "/_/  |_/_____/_____/_/  |_/_/ |_/   \n"
 printf " "
 
 tensorflow_model_server --port=9501 --model_name="player" --enable_batching=true --batching_parameters_file=batch.txt --model_base_path=/model/src/model_server/bot_neurips2019-sl_model/ --tensorflow_session_parallelism=8 --file_system_poll_wait_seconds=3
