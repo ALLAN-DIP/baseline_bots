1bbd0c1cb7cb6ef8e05cb11320f9833abc994054
Kartik Shenoy <kartik.shenoyy@gmail.com>
2022-10-10 10:52:48 -0500

    minor print statement change

--- containers/allan_test_env/run.sh
+++ containers/allan_test_env/run.sh
@@ -4,7 +4,7 @@
 /model/src/model_server/run_model_server.sh &
 
 # ASCII art
-printf "Running ALLAN bot\n"
+printf "Running ALLAN test env\n"
 printf "    ___    __    __    ___    _   __\n"
 printf "   /   |  / /   / /   /   |  / | / /\n"
 printf "  / /| | / /   / /   / /| | /  |/ / \n"
@@ -15,4 +15,4 @@ printf "\n"
 export PYTHONPATH=$PYTHONPATH:/model/src/model_server/research/
 
 # launch bot script
-python /model/src/model_server/baseline_bots/run_bot.py $@
+# python /model/src/model_server/baseline_bots/run_bot.py $@
