1565cfceb0e8b7f5b67c9cb89ac27b0bf0d88838
Wichayaporn Wongkamjan <w.wongkamjan@gmail.com>
2022-10-13 15:35:01 -0500

    Dockerfile update

--- src/baseline_bots/bots/dipnet/transparent_bot.py
+++ src/baseline_bots/bots/dipnet/transparent_bot.py
@@ -58,7 +58,7 @@ class TransparentBot(DipnetBot):
         comms_obj = MessagesData()
 
         parsed_orders = self.parse_messages(rcvd_messages)
-        parsed_orders = [daide_to_dipnet_parsing(order) for order in parsed_orders]
+        parsed_orders = [list(daide_to_dipnet_parsing(order))[0] for order in parsed_orders]
 
         # My orders' messages if not already sent
         if not self.my_orders_informed:
