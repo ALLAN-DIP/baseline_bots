092d864a8d8031bcbfea7a69088a16dbb44ff681
wwongkamjan <wwongkam@MSI.localdomain>
2022-10-16 20:01:57 -0400

    check state value

--- src/baseline_bots/utils.py
+++ src/baseline_bots/utils.py
@@ -432,7 +432,7 @@ def get_best_orders(bot, proposal_order: dict, shared_order: dict):
             unit_orders = get_non_aggressive_orders(
                 unit_orders, bot.power_name, bot.game
             )
-            simulated_game.role = proposer
+            simulated_game.role = bot.power_name
             # set orders as a proposal order
             simulated_game.set_orders(power_name=bot.power_name, orders=unit_orders)
 
