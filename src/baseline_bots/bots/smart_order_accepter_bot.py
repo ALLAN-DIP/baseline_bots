6a5bb506e0a60878cb20226403340d6225a52d14
Feng <59535397+SiddarGu@users.noreply.github.com>
2022-10-21 00:07:31 -0400

    Update smart_order_accepter_bot.py

--- src/baseline_bots/bots/smart_order_accepter_bot.py
+++ src/baseline_bots/bots/smart_order_accepter_bot.py
@@ -15,16 +15,13 @@ from baseline_bots.utils import (
     get_best_orders,
     get_order_tokens,
     get_other_powers,
-    parse_alliance_proposal,
-    parse_arrangement,
-    parse_PRP,
     smart_select_support_proposals,
     REJ
 )
 from baseline_bots.parsing_utils import (
     dipnet_to_daide_parsing,
     daide_to_dipnet_parsing,
-    parse_proposal_messages,
+    parse_proposal_messages
 )
 
 from collections import defaultdict
@@ -62,7 +59,6 @@ class SmartOrderAccepterBot(DipnetBot):
         Add messages to be sent to powers with positive stance.
         These messages would contain factual information about the orders that current power would execute in current round
         """
-
         if orders_list:
             orders_decided = FCT(ORR([XDO(order) for order in dipnet_to_daide_parsing(orders_list, self.game)]))
             if str(orders_decided) != "FCT ()":
@@ -78,31 +74,26 @@ class SmartOrderAccepterBot(DipnetBot):
 
         return msgs_data
 
-    @gen.coroutine
-    def gen_proposal_reply(
-        self, best_proposer: str, prp_orders: dict, messages: MessagesData
-    ) -> MessagesData:
-
+    def gen_proposal_reply(self, best_proposer: str, prp_orders: dict, messages: MessagesData) -> MessagesData: 
         """
         Reply back to allies regarding their proposals whether we follow or not follow
         """
         for proposer, orders in prp_orders.items():
-            if (
-                orders
-                and self.power_name != proposer
-                and self.stance.get_stance()[self.power_name][proposer] > 0
-            ):
+            if orders and self.power_name != proposer and self.stance.get_stance()[self.power_name][proposer]>=0:
                 if proposer == best_proposer:
-                    msg = YES(PRP(ORR(XDO(dipnet_to_daide_parsing(orders)))))
+                    msg = YES(
+                        PRP(ORR([XDO(order) for order in dipnet_to_daide_parsing(orders, self.game)]))
+                    )
                 else:
-                    msg = REJ(PRP(ORR(XDO(dipnet_to_daide_parsing(orders)))))
-                messages.add_message(proposer, str(msg))
+                    msg = REJ(
+                        PRP(ORR([XDO(order) for order in dipnet_to_daide_parsing(orders, self.game)]))
+                    )
+                messages.add_message(
+                    proposer, str(msg)
+                )
         return messages
-
-    def respond_to_invalid_orders(
-        self, invalid_proposal_orders: Dict[str, List[str]], messages_data: MessagesData
-    ) -> None:
-
+    
+    def respond_to_invalid_orders(self, invalid_proposal_orders: Dict[str, List[Tuple[str, str]]], messages_data: MessagesData) -> None:
         """
         The bot responds by HUHing the invalid proposal orders received (this could occur if the move proposed is invalid)
 
@@ -112,14 +103,13 @@ class SmartOrderAccepterBot(DipnetBot):
         if not invalid_proposal_orders:
             return
         for sender in invalid_proposal_orders:
-
             if sender == self.power_name:
                 continue
             message = HUH(PRP(ORR([XDO(order) for order in dipnet_to_daide_parsing(invalid_proposal_orders[sender], self.game, unit_power_tuples_included=True)])))
             messages_data.add_message(
                 sender, str(message)
             )
-
+        
     def respond_to_alliance_messages(self, messages_data: MessagesData) -> None:
         """
         Send YES confirmation messages to all alliance proposals
@@ -308,21 +298,18 @@ class SmartOrderAccepterBot(DipnetBot):
         print("debug: Fetched orders", orders)
 
         # parse the proposal messages received by the bot
-        parsed_messages_dict = parse_proposal_messages(
-            rcvd_messages, self.game, self.power_name
-        )
-        valid_proposal_orders = parsed_messages_dict["valid_proposals"]
-        invalid_proposal_orders = parsed_messages_dict["invalid_proposals"]
-        shared_orders = parsed_messages_dict["shared_orders"]
-        other_orders = parsed_messages_dict["other_orders"]
-        self.alliances = parsed_messages_dict["alliance_proposals"]
+        parsed_messages_dict = parse_proposal_messages(rcvd_messages, self.game, self.power_name)
+        valid_proposal_orders = parsed_messages_dict['valid_proposals']
+        invalid_proposal_orders = parsed_messages_dict['invalid_proposals']
+        shared_orders = parsed_messages_dict['shared_orders']
+        other_orders =  parsed_messages_dict['other_orders']
+        self.alliances =  parsed_messages_dict['alliance_proposals']
 
         # include base order to prp_orders.
         # This is to avoid having double calculation for the best list of orders between (self-generated) base orders vs proposal orders
         # e.g. if we are playing as ENG and the base orders are generated from DipNet, we would want to consider
         # if there is any better proposal orders that has a state value more than ours, then do it. If not, just follow the base orders.
         valid_proposal_orders[self.power_name] = orders
-
 
         best_proposer, best_orders = yield from get_best_orders(self, valid_proposal_orders, shared_orders)
         best_orders, best_proposer = orders, list(self.alliances.keys())[0] if self.alliances else ""
@@ -344,7 +331,6 @@ class SmartOrderAccepterBot(DipnetBot):
                         {msg_foes if msg_foes else 'no one'} as my foes and I am indifferent towards {msg_neutral if msg_neutral else 'no one'}"))
 
         # generate proposal response YES/NO to allies
-
         msgs_data = self.gen_proposal_reply(best_proposer, valid_proposal_orders, msgs_data)
 
         # generate support proposals to allies
@@ -353,4 +339,3 @@ class SmartOrderAccepterBot(DipnetBot):
         print(proposals)
 
         return {"messages": msgs_data, "orders": orders_data.get_list_of_orders()}
-        
