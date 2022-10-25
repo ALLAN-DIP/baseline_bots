2c6e4c4a36c37ff1c9adc07f3eca1ae939e6a53f
Wichayaporn Wongkamjan <w.wongkamjan@gmail.com>
2022-10-25 00:03:25 -0500

    soa random

--- src/baseline_bots/randomize_order.py
+++ src/baseline_bots/randomize_order.py
@@ -177,6 +177,7 @@ def random_convoy_to(order: Tuple) -> Tuple:
     :return: A deviant order (with some chance of being the same order).
     :rtype: Tuple
     """
+    print(order)
     (_, _, amy_loc), _, province, _, (sea_provinces) = order
     sea_provinces = list(reversed(sea_provinces))
     for i, sea in enumerate(
