9af6455730a58c7e10de666bad3636b0d160180c
Wichayaporn Wongkamjan <w.wongkamjan@gmail.com>
2022-10-25 01:34:34 -0500

    debuf

--- src/baseline_bots/randomize_order.py
+++ src/baseline_bots/randomize_order.py
@@ -179,6 +179,7 @@ def random_convoy_to(order: Tuple) -> Tuple:
     """
     print(order)
     (_, _, amy_loc), _, province, _, (sea_provinces) = order
+    sea_provinces = sea_provinces.split()
     sea_provinces = list(reversed(sea_provinces))
     for i, sea in enumerate(
         sea_provinces
