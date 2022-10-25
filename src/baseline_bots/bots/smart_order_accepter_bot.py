5bdb1a81b97cd807e08aad95b60fc6308a6a9d16
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2022-10-25 17:15:53 -0400

    Update smart_order_accepter_bot.py

--- src/baseline_bots/bots/smart_order_accepter_bot.py
+++ src/baseline_bots/bots/smart_order_accepter_bot.py
@@ -556,9 +556,9 @@ class SmartOrderAccepterBot(DipnetBot):
             # fmt: off
             # stance is not yet considered in first phase
             if self.game.get_current_phase()!='S1901M':
-                self.allies = [pow for pow in powers if (pow != self.power_name and powers[pow] > self.ally_threshold)]
-                self.foes = [pow for pow in powers if (pow != self.power_name and powers[pow] < self.enemy_threshold)]
-                self.neutral = [pow for pow in powers if (pow != self.power_name and powers[pow] >= self.enemy_threshold and powers[pow] <= self.ally_threshold)]
+                self.allies = [pow for pow in powers if (pow != self.power_name and powers[pow] >= self.ally_threshold)]
+                self.foes = [pow for pow in powers if (pow != self.power_name and powers[pow] <= self.enemy_threshold)]
+                self.neutral = [pow for pow in powers if (pow != self.power_name and powers[pow] > self.enemy_threshold and powers[pow] < self.ally_threshold)]
                 msg_allies, msg_foes, msg_neutral = ','.join(allies), ','.join(foes), ','.join(neutral)
                 msgs_data.add_message("GLOBAL", str(f"{self.power_name}: From my stance vector perspective, I see {msg_allies if msg_allies else 'no one'} as my allies, \
                                 {msg_foes if msg_foes else 'no one'} as my foes and I am indifferent towards {msg_neutral if msg_neutral else 'no one'}"))
