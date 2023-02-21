a55f5476fe8d855049520be7cd48e2e4c74451ad
Alex Hedges <ahedges@isi.edu>
2023-02-21 16:47:27 -0500

    Add scripts to more easily run games
    I created `run_games.sh` loosely based on a script on TACC outside of
    any repository. This script should be usable as a direct but more
    powerful substitute.
    
    `create_game.py` is a cleaned-up version of a script of the same name
    from https://github.com/SHADE-AI/diplomacy-playground/.

---
+++ scripts/create_game.py
@@ -0,0 +1,90 @@
+# SPDX-FileCopyrightText: 2022, Texas Advanced Computing Center
+# SPDX-License-Identifier: BSD-3-Clause
+"""Create Diplomacy games programmatically."""
+# Based on https://github.com/SHADE-AI/diplomacy-playground/blob/989ec7be748257324a22132e632ac4927b8cb6c2/scripts/create_game.py
+import argparse
+import asyncio
+import json
+from typing import Optional, Sequence
+
+from diplomacy.client.connection import connect
+
+DEFAULT_RULES = ("REAL_TIME", "POWER_CHOICE")
+
+
+async def create_game(
+    game_id: str,
+    rules: Sequence[str] = DEFAULT_RULES,
+    deadline: int = 0,
+    n_controls: int = 7,
+    password: Optional[str] = None,
+    hostname: str = "localhost",
+    port: int = 8432,
+) -> None:
+    """Creates a game on the Diplomacy server"""
+    connection = await connect(hostname, port)
+    channel = await connection.authenticate("random_user", "password")
+
+    game = await channel.create_game(
+        game_id=game_id,
+        rules=rules,
+        deadline=deadline,
+        n_controls=n_controls,
+        registration_password=password,
+    )
+
+    game_data = {
+        "id": game.game_id,
+        "deadline": game.deadline,
+        "map_name": game.map_name,
+        "registration_password": game.registration_password,
+        "rules": game.rules,
+        "n_controls": n_controls,
+        "status": game.status,
+        "daide_port": game.daide_port,
+    }
+    print(json.dumps(game_data, ensure_ascii=False, indent=4))
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--game_id", type=str, required=True, help="Game ID.")
+    parser.add_argument("--rules", nargs="+", default=DEFAULT_RULES, help="Game rules.")
+    parser.add_argument(
+        "--deadline", type=int, default=0, help="Turn deadline in seconds."
+    )
+    parser.add_argument(
+        "--n_controls",
+        type=int,
+        default=7,
+        help="Number of controlled powers (default: %(default)s)",
+    )
+    parser.add_argument("--password", type=str, help="Game password.")
+    parser.add_argument(
+        "--host", type=str, default="localhost", help="Server hostname."
+    )
+    parser.add_argument("--port", type=int, default=8432, help="Server port.")
+    args = parser.parse_args()
+
+    if args.deadline < 0:
+        raise ValueError("--deadline cannot be negative")
+    if args.n_controls < 0:
+        raise ValueError("--n_controls cannot be negative")
+    if args.n_controls > 7:
+        raise ValueError("--n_controls cannot be greater than 7")
+
+    asyncio.run(
+        create_game(
+            game_id=args.game_id,
+            rules=args.rules,
+            deadline=args.deadline,
+            n_controls=args.n_controls,
+            password=args.password,
+            hostname=args.host,
+            port=args.port,
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/run_games.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+CUR_FILE=$(basename "$(readlink -f "$0")")
+IS_ON_TACC=$(
+  [[ $(hostname) =~ .*\.tacc\.utexas\.edu ]]
+  echo $?
+)
+
+if [ "${1:-}" = "--help" ]; then
+  echo "usage: bash $CUR_FILE [--runner RUNNER] [--game-id GAME_ID] [--agent AGENT] [bot args]" >&2
+  echo >&2
+  echo 'Arguments must be provided in this order.' >&2
+  echo 'RUNNER must be either "docker" or "singularity".' >&2
+  echo 'It defaults to "singularity" on TACC but "docker" everywhere else.' >&2
+  echo 'If GAME_ID is not provided, then one will be generated automatically.' >&2
+  echo 'If AGENT is not provided, then a default will be chosen depending on the runner and host.' >&2
+  echo 'Bot args are passed when running the container.' >&2
+  exit 1
+fi
+
+if [ -n "${1:-}" ] && [ "--runner" = "$1" ]; then
+  RUNNER=$2
+  shift 2
+elif [[ $IS_ON_TACC -eq 0 ]]; then
+  RUNNER=singularity
+else
+  RUNNER=docker
+fi
+
+if [[ $RUNNER == singularity ]]; then
+  if ! command -v singularity 1>/dev/null 2>&1; then
+    echo -n "Singularity is not installed." >&2
+    if [[ $IS_ON_TACC -eq 0 ]]; then
+      echo -n " Run \`module load tacc-singularity\`." >&2
+    fi
+    echo >&2
+    exit 2
+  fi
+  RUN_CMD='singularity run'
+elif [[ $RUNNER == docker ]]; then
+  if ! command -v docker 1>/dev/null 2>&1; then
+    echo "Docker is not installed." >&2
+    exit 2
+  fi
+  RUN_CMD='docker run --rm'
+else
+  echo "Provided runner not recognized." >&2
+  exit 2
+fi
+
+if [ -n "${1:-}" ] && [ "--game-id" = "$1" ]; then
+  GAME_ID=$2
+  shift 2
+else
+  GAME_ID=
+fi
+
+if [ -n "${1:-}" ] && [ "--agent" = "$1" ]; then
+  AGENT=$2
+  shift 2
+else
+  if [[ $RUNNER == singularity ]] && [[ $IS_ON_TACC -eq 0 ]]; then
+    # Automatically find latest version downloaded on TACC
+    AGENT=$(
+      find /corral/projects/DARPA-SHADE/Milestone_4/UMD/ \
+        -maxdepth 1 -type f -name 'allan_bots_*.sif' |
+        sort | tail -1
+    )
+  else
+    AGENT=allan_dip_bot
+  fi
+fi
+
+HOST=shade-dev.tacc.utexas.edu
+OPTS="--bot_type SmartOrderAccepterBot"
+
+if [[ -z $GAME_ID ]]; then
+  # Calculate path based on script's expected location
+  CREATE_GAME_SCRIPT="$(dirname "$(readlink --canonicalize "$0")")"/create_game.py
+  if [[ ! -f $CREATE_GAME_SCRIPT ]]; then
+    echo 'Cannot find "create_game.py" script. The ID of an existing game must be provided.' >&2
+    exit 2
+  fi
+
+  CUR_ISO_DATE=$(date -u +'%Y_%m_%d_%H_%M_%S')
+  GAME_ID=${USER}_$CUR_ISO_DATE
+
+  python "$CREATE_GAME_SCRIPT" --game_id "$GAME_ID" --deadline 300 --host $HOST
+fi
+
+LOG_DIR=logs/"$GAME_ID"
+mkdir -p "$LOG_DIR"
+
+POWERS=(AUSTRIA ENGLAND GERMANY FRANCE ITALY RUSSIA TURKEY)
+for POWER in "${POWERS[@]}"; do
+  $RUN_CMD "$AGENT" --host $HOST --game_id "$GAME_ID" --power "$POWER" "$OPTS" "$@" \
+    &>"$LOG_DIR"/"$POWER".txt &
+  sleep 4
+done
