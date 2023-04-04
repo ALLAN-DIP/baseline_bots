8ae81059785c99ae439d874798d891ace55a509c
Alex Hedges <ahedges@isi.edu>
2023-04-04 17:39:31 -0400

    Remove unused `dipnet.py` test file

--- tests/bot_tests/dipnet.py
+++
@@ -1,6 +0,0 @@
-from baseline_bots.bots.dipnet.selectively_transparent_bot import (
-    SelectivelyTransparentBot,
-)
-from baseline_bots.gameplay_framework import GamePlay
-
-game_play = GamePlay(None, [SelectivelyTransparentBot, SelectivelyTransparentBot], 3)
