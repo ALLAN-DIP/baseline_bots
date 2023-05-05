69728fd0b4845508f43bafb427d7defb6281ea47
Alex Hedges <ahedges@isi.edu>
2023-05-05 18:37:58 -0400

    Remove unused non-DipNet bots
    It would take a lot of effort to make all of these produce valid DAIDE,
    and bots that do not use DipNet would not make good baselines when
    trying to determine the effectiveness of using press.

--- tests/bot_tests/bots_test.py
+++ tests/bot_tests/bots_test.py
@@ -10,14 +10,6 @@ from baseline_bots.bots.dipnet.selectively_transparent_bot import (
     SelectivelyTransparentBot,
 )
 from baseline_bots.bots.dipnet.transparent_bot import TransparentBot
-from baseline_bots.bots.loyal_bot import LoyalBot
-from baseline_bots.bots.pushover_bot import PushoverBot
-from baseline_bots.bots.random_allier_proposer_bot import RandomAllierProposerBot
-from baseline_bots.bots.random_honest_bot import RandomHonestBot
-from baseline_bots.bots.random_honest_order_accepter_bot import (
-    RandomHonestOrderAccepterBot,
-)
-from baseline_bots.bots.random_no_press import RandomNoPressBot
 from baseline_bots.bots.random_proposer_bot import (
     RandomProposerBot,
     RandomProposerBot_AsyncBot,
@@ -25,10 +17,6 @@ from baseline_bots.bots.random_proposer_bot import (
 
 
 class TestOtherBots(AsyncTestCase):
-    def test_no_press_vs_no_press(self):
-        game_play = GamePlay(None, [RandomNoPressBot, RandomNoPressBot], 3, True)
-        game_play.play()
-
     @gen_test
     def test_no_press_dip_vs_no_press_dip(self):
         game_play = GamePlayAsync(None, [NoPressDipBot, NoPressDipBot], 3, True)
@@ -61,26 +49,4 @@ class TestOtherBots(AsyncTestCase):
 
     def test_random_proposer_vs_random_proposer(self):
         game_play = GamePlay(None, [RandomProposerBot, RandomProposerBot], 3, True)
-        game_play.play()
-
-    def test_loyal_vs_random_proposer(self):
-        game_play = GamePlay(None, [LoyalBot, RandomProposerBot], 3)
-        game_play.play()
-
-    def test_pushover_vs_random_proposer(self):
-        game_play = GamePlay(None, [PushoverBot, RandomProposerBot], 3)
-        game_play.play()
-
-    def test_random_allier_proposer_vs_random_allier_proposer(self):
-        game_play = GamePlay(
-            None, [RandomAllierProposerBot, RandomAllierProposerBot], 3
-        )
-        game_play.play()
-
-    def test_random_honest_vs_random_honest(self):
-        game_play = GamePlay(None, [RandomHonestBot, RandomHonestBot], 3)
-        game_play.play()
-
-    def test_random_proposer_vs_random_honest_order_accepter(self):
-        game_play = GamePlay(None, [RandomProposerBot, RandomHonestOrderAccepterBot], 3)
         game_play.play()
