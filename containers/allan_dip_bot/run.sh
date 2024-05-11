59f9d5a08b35d3171d8d5ecc79e9739366bde489
Alex Hedges <git@alexhedges.dev>
2024-05-10 20:20:19 -0400

    Remove DipNet setup from `Dockerfile`

--- containers/allan_dip_bot/run.sh
+++ containers/allan_dip_bot/run.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-# launch model server
-/model/src/model_server/baseline_bots/containers/allan_dip_bot/run_model_server.sh &
-
 # ASCII art
 printf "Running ALLAN bot\n"
 printf "    ___    __    __    ___    _   __\n"
@@ -13,8 +10,6 @@ printf "  / /| | / /   / /   / /| | /  |/ / \n"
 printf " / ___ |/ /___/ /___/ ___ |/ /|  /  \n"
 printf "/_/  |_/_____/_____/_/  |_/_/ |_/   \n"
 printf "\n"
-
-export PYTHONPATH=$PYTHONPATH:/model/src/model_server/research/
 
 # launch bot script
 python /model/src/model_server/baseline_bots/containers/allan_dip_bot/run_bot.py "$@"
