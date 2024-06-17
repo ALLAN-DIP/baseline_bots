7eaf90cf44c6aaa89bb71c9b0272dab775d2798e
Alex Hedges <git@alexhedges.dev>
2024-06-17 19:37:55 -0400

    manual formatting fixes

--- src/baseline_bots/parsing_utils.py
+++ src/baseline_bots/parsing_utils.py
@@ -226,7 +226,7 @@ def dipnet_to_daide_parsing(
             raise
         except Exception as ex:
             logger.exception(
-                "ALLAN: error from %s.%s()\n" "\tOrder with error: %r\n" "\tSet of orders: %s",
+                "ALLAN: error from %s.%s()\n\tOrder with error: %r\n\tSet of orders: %s",
                 __name__,
                 dipnet_to_daide_parsing.__name__,
                 " ".join(dipnet_order_tokens),
@@ -313,7 +313,7 @@ def daide_to_dipnet_parsing(daide_order: Command) -> Optional[Tuple[str, str]]:
         raise
     except Exception as ex:
         logger.exception(
-            "ALLAN: error from %s.%s\n" "\tCould not convert DAIDE command %r to DipNet format",
+            "ALLAN: error from %s.%s\n\tCould not convert DAIDE command %r to DipNet format",
             __name__,
             daide_to_dipnet_parsing.__name__,
             str(daide_order),
@@ -347,7 +347,7 @@ def parse_proposal_messages(
         # Extract messages containing PRP string
         order_msgs = [msg for msg in rcvd_messages if isinstance(parse_daide(msg.message), PRP)]
         logger.info(
-            "Received %d PRP messages: " "%s",
+            "Received %d PRP messages: %s",
             len(order_msgs),
             [(order_msg.sender, order_msg.message) for order_msg in order_msgs],
         )
@@ -386,7 +386,7 @@ def parse_proposal_messages(
                 raise
             except Exception as ex:
                 logger.exception(
-                    "ALLAN: error from %s.%s()\n" "\tUnexpected proposal message format: %r",
+                    "ALLAN: error from %s.%s()\n\tUnexpected proposal message format: %r",
                     __name__,
                     parse_proposal_messages.__name__,
                     order_msg.message,
@@ -426,7 +426,7 @@ def parse_proposal_messages(
 
         if other_orders:
             logger.info(
-                "ALLAN: Found other orders while extracting proposal messages: " "%s",
+                "ALLAN: Found other orders while extracting proposal messages: %s",
                 [msg.message for msg in order_msgs],
             )
             logger.info("ALLAN: Other orders found: %s", dict(other_orders))
@@ -443,7 +443,7 @@ def parse_proposal_messages(
         raise
     except Exception as ex:
         logger.exception(
-            "ALLAN: error from %s.%s()\n" "\tReceived messages: %s",
+            "ALLAN: error from %s.%s()\n\tReceived messages: %s",
             __name__,
             parse_proposal_messages.__name__,
             rcvd_messages,
