63fc118e4731382fc9139978b7e96fd62a92aa83
Alex Hedges <git@alexhedges.dev>
2024-06-17 18:26:49 -0400

    manual ruff fixes

--- src/baseline_bots/bots/__init__.py
+++ src/baseline_bots/bots/__init__.py
@@ -1,2 +1,2 @@
-from baseline_bots.bots.baseline_bot import BaselineBot
-from baseline_bots.bots.random_proposer_bot import RandomProposerBot
+from baseline_bots.bots.baseline_bot import BaselineBot as BaselineBot
+from baseline_bots.bots.random_proposer_bot import RandomProposerBot as RandomProposerBot
