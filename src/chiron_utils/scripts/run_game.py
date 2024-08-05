42a8b438ff8abda395a33753a301d501a79a79d4
Alex Hedges <git@alexhedges.dev>
2024-08-05 18:58:22 -0400

    Use `game_` prefix for game JSON output file

--- src/chiron_utils/scripts/run_game.py
+++ src/chiron_utils/scripts/run_game.py
@@ -89,7 +89,7 @@ def main() -> None:
         }
     game_record = asyncio.run(download_game(game_id, hostname=host))
     output = {"run_output": run_output, "game_record": game_record}
-    output_file = data_dir / f"record_{game_id}.json"
+    output_file = data_dir / f"game_{game_id}.json"
     with open(output_file, "w", encoding="utf-8") as file:
         json.dump(output, file, ensure_ascii=False, indent=2)
         file.write("\n")
