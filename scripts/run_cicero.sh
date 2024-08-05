2c858ae05330d5e82d55db334386b7d40fddde61
Alex Hedges <git@alexhedges.dev>
2024-08-05 19:32:14 -0400

    Copy changes from `run_antony.sh` to `run-cicero.sh`
    This is going to be needed now that I'll be running CICERO.

--- scripts/run_cicero.sh
+++ scripts/run_cicero.sh
@@ -2,21 +2,36 @@
 
 set -euxo pipefail
 
+GAME_ID=$1
+HOST=$2
+POWER=$3
+CUDA_NUM=$4
+shift 4
+
 export WORK=$PWD
-CICERO=$(realpath cicero/)
-export CICERO
 REPO=$(realpath diplomacy_cicero/)
 export REPO
-export GAME_COMMAND="python fairdiplomacy_external/mila_api.py --game_id $1 --host $2 --power $3 --game_type 2"
-export CUDA_VISIBLE_DEVICES=$4
+CICERO=$(realpath cicero/)
+export CICERO
+export GAME_COMMAND=(
+  python fairdiplomacy_external/mila_api.py
+  --game_id "$GAME_ID"
+  --host "$HOST"
+  --power "$POWER"
+  --game_type 2
+)
 
-cd "$REPO"
+# Sets `CUDA_VISIBLE_DEVICES` inside the container
+# From https://docs.sylabs.io/guides/3.11/user-guide/gpu.html#multiple-gpus
+export SINGULARITYENV_CUDA_VISIBLE_DEVICES=$CUDA_NUM
 
-singularity run --compat --nv \
+time singularity run --compat --nv \
+  --env OPENBLAS_NUM_THREADS=4 \
+  --bind "$REPO"/fairdiplomacy/AMR/:/diplomacy_cicero/fairdiplomacy/AMR/ \
   --bind "$REPO"/fairdiplomacy_external:/diplomacy_cicero/fairdiplomacy_external \
   --bind "$REPO"/parlai_diplomacy:/diplomacy_cicero/parlai_diplomacy \
-  --bind "$REPO"/fairdiplomacy/AMR/:/diplomacy_cicero/fairdiplomacy/AMR/ \
   --bind "$CICERO"/agents:/diplomacy_cicero/conf/common/agents \
   --bind "$CICERO"/models:/diplomacy_cicero/models \
   --bind "$CICERO"/gpt2:/usr/local/lib/python3.7/site-packages/data/gpt2 \
-  --pwd /diplomacy_cicero ../cicero_latest.sif bash -c "$GAME_COMMAND"
+  --pwd /diplomacy_cicero \
+  cicero_latest.sif "${GAME_COMMAND[@]}" "$@"
